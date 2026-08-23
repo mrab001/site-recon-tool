c7e3b71ca206a6283681e4906e20eedcd70b6c6e
mrab001 <ajo10@tutamail.com>
2026-08-23 18:35:20 +0300

    Update recon-ab0.py

--- recon-ab0.py
+++ recon-ab0.py
@@ -5,6 +5,12 @@ from urllib.parse import urlparse, urljoin
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
+####################################################################################################
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+####################################################################################################
+
 
 def Clean_url():
     User_Agent = {
@@ -22,12 +28,14 @@ def Clean_url():
         else:
             response = requests.get(url, headers=User_Agent, timeout=5)
     except Exception as error:
-        print(f"[-] error: {error}")
-        exit()
+        print(f"{RED}[-] error: {error}{RESET}")
+        Clean_url()
     return response, url
 
 
 response, url = Clean_url()
+parsed_main_url = urlparse(url)
+setop = set()
 
 header = [
     "X-Content-Type-Options",
@@ -44,20 +52,18 @@ header = [
     "X-Generator"
 ]
 
-####################################################################################################
-GREEN = '\033[92m'
-RED = '\033[91m'
-RESET = '\033[0m'
-####################################################################################################
-
 
 def Scanning_headers():
-
-    for headers in header:
-        if headers in response.headers:
-            print(f"{GREEN}[+] {headers}: {response.headers[headers]}{RESET}")
-        else:
-            print(f"{RED}[-] Not found: {headers}{RESET}")
+    try:
+        for headers in header:
+            if headers in response.headers:
+                print(
+                    f"{GREEN}[+] {headers}: {response.headers[headers]}{RESET}")
+            else:
+                print(f"{RED}[-] Not found: {headers}{RESET}")
+    except Exception as error:
+        print(f"{RED}[-] error: {error}{RESET}")
+        Clean_url()
 
 
 Scanning_headers()
@@ -68,7 +74,6 @@ print("=" * 60)
 def path_scan():
     soup = BeautifulSoup(response.text, 'html.parser')
     script = soup.select('[src], [href]')
-    setpath = set()
 
     if not script:
         print("[-] No paths were found")
@@ -77,8 +82,8 @@ def path_scan():
     for scripts in script:
         path = scripts.get('src') or scripts.get('href')
 
-        if path and path not in setpath:
-            setpath.add(path)
+        if path and path not in setop:
+            setop.add(path)
             join_url = urljoin(url, path)
             parsed_url = urlparse(join_url)
             root_domain = parsed_url.netloc
@@ -91,32 +96,57 @@ path_scan()
 
 print("=" * 60)
 
-url_2 = urlparse(url)
-loc = socket.gethostbyname(url_2.netloc)
-print("\n" + "=" * 40)
-print(f"[+] IP Address: [{loc}]")
-print("=" * 40)
-print("[1] Scan ports from 1 to 1024 (Well-known ports)")
-print("[2] Scan ports from 1 to 49151 (Registered ports)")
-print("[3] Scan ports from 1 to 65535 (All ports)")
-print("[4] Skip port scanning")
-print("=" * 40)
-choice = int(input("Select an option (1-4): ").strip())
-if choice == 1:
-    ports = range(1, 1025)
-    print(f"{GREEN}[+] Scanning ports 1 to 1024...{RESET}")
-elif choice == 2:
-    ports = range(1, 49152)
-    print(f"{GREEN}[+] Scanning ports 1 to 49151...{RESET}")
-elif choice == 3:
-    ports = range(1, 65536)
-    print(f"{GREEN}[+] Scanning ports 1 to 65535...{RESET}")
-elif choice == 4:
-    print(f"{RED}[-] Port scanning skipped{RESET}")
-    exit()
-else:
-    print(f"{RED}[-] Invalid choice, skipping port scan{RESET}")
-    exit()
+
+def Subdomain():
+    url_1 = parsed_main_url.netloc
+    Sub = f"https://api.hackertarget.com/hostsearch/?q={url_1}"
+    get = requests.get(Sub)
+    if get.status_code == 200:
+        get_text = get.text.splitlines()
+        for get_all in get_text:
+            if get_all and get_all not in setop:
+                setop.add(get_all)
+                print(f"Subdomain: {get_all.split(',')[0]}")
+                print(f"  └── IP Address: {get_all.split(',')[1]}\n")
+
+
+Subdomain()
+
+
+def Ask():
+    ip = socket.gethostbyname(parsed_main_url.netloc)
+    print("\n" + "=" * 40)
+    print(f"[+] IP Address: [{ip}]")
+    print("=" * 40)
+    print("[1] Scan ports from 1 to 1024 (Well-known ports)")
+    print("[2] Scan ports from 1 to 49151 (Registered ports)")
+    print("[3] Scan ports from 1 to 65535 (All ports)")
+    print("[4] Skip port scanning")
+    print("=" * 40)
+    try:
+        choice = int(input("Select an option (1-4): ").strip())
+        if choice == 1:
+            ports = range(1, 1025)
+            print(f"{GREEN}[+] Scanning ports 1 to 1024...{RESET}")
+        elif choice == 2:
+            ports = range(1, 49152)
+            print(f"{GREEN}[+] Scanning ports 1 to 49151...{RESET}")
+        elif choice == 3:
+            ports = range(1, 65536)
+            print(f"{GREEN}[+] Scanning ports 1 to 65535...{RESET}")
+        elif choice == 4:
+            print(f"{RED}[-] Port scanning skipped{RESET}")
+            exit()
+        else:
+            print(f"{RED}[-] Invalid choice{RESET}")
+            Ask()
+    except Exception as error:
+        print(f"{RED}[-] error : {error}{RESET}")
+        Ask()
+    return ports, ip
+
+
+ports, ip = Ask()
 
 print_lock = threading.Lock()
 
@@ -125,7 +155,7 @@ def sok(port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(1)
-            connect = s.connect_ex((loc, port))
+            connect = s.connect_ex((ip, port))
             if connect == 0:
                 with print_lock:
                     print(f"{GREEN}[+] port  [{port}] is open{RESET}")
